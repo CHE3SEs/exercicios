54f1eb6bacc0c748bf4885d375997f5dbcb571c4
CHE3SE <84106761+CHE3SEs@users.noreply.github.com>
2023-03-23 15:08:00 -0300

    Add files via upload

---
+++ Introdução a Python.py
@@ -0,0 +1,60 @@
+#Exercicio 1
+print("Bem-vindo ao mundo da programação!")
+
+#Exercicio 2
+a = int((input("escreva um numero inteiro: ")))
+print("o dobro do seu numero é: ", a * 2)
+
+#Exercicio 3
+c = float((input("escreva um numero real/float: ")))
+d = float((input("escreva outro numero real/float: ")))
+
+print("a soma dos seus numeros é: ", c + d)
+
+
+#Exercicio 4
+base = float((input("qual a base do triangulo? ")))
+altura = float((input("qual a altura do triangulo? ")))
+
+print("a base do seu triangulo é: ", (base * altura)/2)
+
+
+#Exercicio 5
+nome= (input("qual o seu nome? "))
+sobrenome= (input("qual o seu sobrenome? "))
+print(sobrenome, ", ", nome)
+
+
+#Exercicio 6
+
+dolar = float(input("qual o cotação do dolar? "))
+carteira = float(input("quantos dolares voce tem? "))
+reais = carteira * dolar
+
+print("voce tem: ", reais, "Reais")
+
+#Exercicio 7 
+
+kg = 25
+comida = float((input("sua refeição pesa quantos kg? ")))
+
+print("seu prato custa", comida * kg, "reais")
+
+#Exercicio 8
+
+aluno = input("qual o seu nome? ")
+n1 = float(input("qual foi a primeria nota? "))
+n2 = float(input("qual foi a segunda nota? "))
+n3 = float(input("qual foi a terceira nota? "))
+media= (n1 + n2 + n3)/ 3
+
+print(f'o alunldo {aluno}, a media dele foi {media}')
+
+
+
+
+
+
+
+
+
